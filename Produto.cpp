48fcec9db0324c20063917ead83d971e39712d43
Daniel Medeiros <danielmmf@gmail.com>
2017-09-10 20:07:05 -0300

    initial commit

---
+++ Produto.cpp
@@ -0,0 +1,11 @@
+#include "Produto.h"
+
+int Produto::setCodigo(int cod){
+   return (cod);
+}
+
+ 
+
+int Produto::getCodigo(){
+   return 10;
+}
---
+++ Produto.h
@@ -0,0 +1,12 @@
+#ifndef PRODUTO_H__
+
+#define PRODUTO_H__
+
+class Produto{
+
+public:
+   int setCodigo(int cod);
+   int getCodigo();
+};
+
+#endif
